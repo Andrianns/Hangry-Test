50bcc210047269c4e4f2d469a149ae8c23d222fd
Andrian Aji <andrianaji06@gmail.com>
2025-01-20 00:06:07 +0700

    fix path

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const authentication = require('../middleware/authentication');
 const userRouter = require('./user');
 const ruleRouter = require('./rule');
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../../index.html'));
 });
 router.use('/user', userRouter);
 router.use(authentication);
