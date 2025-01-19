9fc2fee92339102c6aa779e06de0703e02d4fb79
Andrianns <andrianaji06@gmail.com>
2025-01-19 12:45:23 +0700

    add home api

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const path = require('path');
+
 // const authentication = require('../middleware/authentication');
 const userRouter = require('./user');
 const ruleRouter = require('./rule');
-
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 router.use('/user', userRouter);
 router.use('/rule', ruleRouter);
+
 //authentication
 
 module.exports = router;
